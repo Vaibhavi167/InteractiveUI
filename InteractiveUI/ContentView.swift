41524526d800f4544518f556cd3b6fe7bec39228
Vaibhavi Bandaru <vaibhavi.bandaru7@gmail.com>
2025-07-09 13:46:57 -0400

    Added the background color aspect as a comment

--- InteractiveUI/ContentView.swift
+++ InteractiveUI/ContentView.swift
@@ -12,25 +12,34 @@ struct ContentView: View {
     @State private var name = ""
     @State private var textTitle = "What is your name?"
     
+    
     var body: some View {
-        VStack {
-            Text(textTitle)
-                .font(.title)
+        
+        ZStack
+        {
+            //Color.cyan
+            VStack {
+                Text(textTitle)
+                    .font(.title)
+                    
+                TextField("Type your name here...", text: $name)
+                    .multilineTextAlignment(.center)
+                    .font(.title)
+                    .border(Color.gray, width: 3)
                 
-            TextField("Type your name here...", text: $name)
-                .multilineTextAlignment(.center)
-                .font(.title)
-                .border(Color.gray, width: 3)
-            
-            Button("Submit Name") {
-                textTitle = "Welcome \(name)!"
-            }
-            .font(.title2)
-            .buttonStyle(BorderedProminentButtonStyle())
-            .tint(.purple)
+                Button("Submit Name") {
+                    textTitle = "Welcome \(name)!"
+                }
+                .font(.title2)
+                .buttonStyle(BorderedProminentButtonStyle())
+                .tint(.purple)
 
+            }
+            .padding()
         }
-        .padding()
+        
+        
+        
     }
 }
 
